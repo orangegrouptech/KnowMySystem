770e398a54d6a3eb97b2075f0f6ead738fc0373b
Orange Group Tech <63195743+orangegrouptech@users.noreply.github.com>
2024-10-06 00:31:04 +0800

    Cleaned dependencies

--- Pages/HardwareSpecificationsPage.xaml.cs
+++ Pages/HardwareSpecificationsPage.xaml.cs
@@ -3,8 +3,6 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Management;
-using System.Threading.Tasks;
-using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace KnowMySystem
